c5c385e9dc039b8f7ab034be87d6a09c9d316343
PeterHellmuth <10523853+PeterHellmuth@users.noreply.github.com>
2023-09-27 11:10:24 -0500

    Add functionality to add categories

--- src/domGenerate.js
+++ src/domGenerate.js
@@ -1,19 +1,62 @@
-export {generateCategories, generateTodoItems}
+export {generateCategories, generateTodoItems, generateCategoryDialog, generateTodoDialog}
 import { project, todoItem } from "./todo"
 
-function generateCategories(project, appendTo){
+function generateCategories(project, appendTo, clearFirst = false){
+    if(clearFirst){
+        while (appendTo.hasChildNodes()) {
+            appendTo.removeChild(appendTo.lastChild)
+        }
+    }
     project.categories.forEach((category) => appendElem(appendTo, "h3", category, "category-header"));
-    appendElem(appendTo, "button", "+","add-category-button");
+    return appendElem(appendTo, "button", "+","add-category-button");
 }
 
-function generateTodoItems(project, appendTo){
+function generateTodoItems(project, appendTo, clearFirst = false){
+    if(clearFirst){
+        while (appendTo.hasChildNodes()) {
+            appendTo.removeChild(appendTo.lastChild)
+        }
+    }
     project.todoItems.forEach((todoItem) => {
         let newItem = appendElem(appendTo, "p", todoItem.title, "todo-item");
         newItem.style.gridColumn = project.getGridColumnIndex(newItem.category);
     });
 
-    appendElem(appendTo, "button", "+","add-todo-button");
+    return appendElem(appendTo, "button", "+","add-todo-button");
 }
+
+
+function generateTodoDialog(parentElem, todoItem=null){
+    if(todoItem){
+
+    }
+    const dialogBox = document.createElement("dialog");
+    dialogBox.classList.add("dialog-box");
+    appendElem(dialogBox, "form", null, "todo-form");
+
+}
+
+function generateCategoryDialog(parentElem, category=null){
+    let currentValue = "";
+    if(category){
+        currentValue = category
+    }
+
+    const dialogBox = document.createElement("dialog");
+    dialogBox.classList.add("dialog-box");
+    let dialogForm = appendElem(dialogBox, "form", null, "category-form");
+    appendElem(dialogForm, "h2", "Category name:");
+    let inputField = appendElem(dialogForm, "input", null, null, "category-name-input");
+    inputField.value = currentValue;
+    let submitButton = appendElem(dialogForm, "button", "Submit");
+    submitButton.type="submit";
+    parentElem.appendChild(dialogBox);
+    dialogBox.showModal();
+    return submitButton;
+}
+
+
+
 
 function appendElem(parentElem, type, innerText = null, classIn = null, id = null){
     let childElem = document.createElement(`${type}`);
--- src/index.js
+++ src/index.js
@@ -1,18 +1,49 @@
 import './style.css';
 import { todoItem, project } from './todo';
-import { generateCategories, generateTodoItems } from './domGenerate';
+import { generateCategories, generateTodoItems, generateTodoDialog, generateCategoryDialog } from './domGenerate';
 
 const categoriesDiv = document.getElementById("categories");
 const todoItemsDiv = document.getElementById("todo-items");
 
+let currentProject = null;
 let defaultCat = "Things to get done.";
 let defaultProject = new project("Default project", "This is the default project, feel free to add items or make a new project.");
-defaultProject.addCategory(defaultCat);
+currentProject = defaultProject;
 
+currentProject.addCategory(defaultCat);
 let firstTodo = new todoItem("Something", "this is a thing", "tomorrow", 1, defaultCat);
-defaultProject.addTodoItem(firstTodo);
+currentProject.addTodoItem(firstTodo);
+let addCategoryButton = generateCategories(currentProject, categoriesDiv);
+let addTodoButton = generateTodoItems(currentProject, todoItemsDiv);
 
-generateCategories(defaultProject, categoriesDiv);
-generateTodoItems(defaultProject, todoItemsDiv);
 
+addCategoryButton.addEventListener("click", addCategory);
+addTodoButton.addEventListener("click", addTodo);
+
+
+
+
+function addCategory(){
+    let submitButton = generateCategoryDialog(todoItemsDiv);
+    submitButton.addEventListener("click", categorySubmitted);
+}
+
+function addTodo(){
+    generateTodoDialog(todoItemsDiv);
+}
+
+
+function categorySubmitted(event){
+    event.preventDefault();
+
+    let categoryNameInput = document.getElementById("category-name-input").value;
+    if(categoryNameInput){
+        currentProject.addCategory(categoryNameInput);
+        addCategoryButton = generateCategories(currentProject, categoriesDiv, true);
+        addCategoryButton.addEventListener("click", addCategory);
+    }
+
+    let dialogBox = event.target.parentElement.parentElement;
+    dialogBox.close();
+}
 
